ba1c46bb74f1929075a6d4a043a1503b135fbc7c
Kaushik <kaushikred293@gmail.com>
2021-02-10 12:46:36 +0530

    Added a canActivate module to assist with unidentified urls, also added a 404 page component, main changes in routes.ts , eventrouteacitvator and improted new module - ActivatedRouteSnapshot (read more) , also work on CanActivate. The + in front route.params['id'] tells that the variable returned is an id , similar use in eventdetails component under ngOnInit

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,12 +2,14 @@ import {Routes} from '@angular/router'
 import {EventsListComponent} from './events/events-list.component'
 import {EventDetailsComponent} from './events/event-details/event-details.component'
 import { CreateEventComponent } from './events/create-event.component'
+import { Error404Component } from './errors/404.components'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 
 export const appRoutes:Routes = [
     {path: 'events/new', component: CreateEventComponent}, // moved to the top so brower doesn't confuse with events/:id, only goes here if url shows new
     {path: 'events', component: EventsListComponent},
-    {path: 'events/:id', component: EventDetailsComponent},
-    {path: '', redirectTo: '/events', pathMatch: 'full'}
-    
-    
+    {path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
+    {path: '', redirectTo: '/events', pathMatch: 'full'},
+    // event path set is referenced in other files using the router module or routerLink, this is an easier method compared to href
+    {path: '404', component:Error404Component}
 ]

15c758f8169193f69c99edd9c2df2ab1879fc6c1
Kaushik <kaushikred293@gmail.com>
2021-02-12 13:44:27 +0530

    added create event page and used more validators with template based forms > also edited event service .ts to save another event after validation > edited create event html to connect with logic in create event.ts to enter , validate and save events

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -2,26 +2,39 @@ import { importType } from "@angular/compiler/src/output/output_ast";
 
 import {Component} from '@angular/core'
 import { Router } from "@angular/router";
+import { EventService } from "./shared";
 
 @Component ({
-    template: `
-    <h1> New Events </h1>
-    <hr>
-    <div class = "col-md-6">
-        <h3> [Create Event form will go here] </h3>
-        <br/>
-        <br/>
-        <button type="submit" class = "btn btn-primary"> Save </button>
-        <button type="button" class = "btn btn-default" (click) = "cancel()"> Cancel </button>` //[routerLink] = "['/events']" adding this to the tag also works 
+    templateUrl: "create-event.component.html",
+    //[routerLink] = "['/events']" adding this to the tag also works 
+    styles: [`
+    em {float:right; color:#E05C65; padding-left: 10px;}
+    .error input {background-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color:#999; }
+    .error :ms-input-placeholder { color: #999; }
+  `]
 })
 
 export class CreateEventComponent {
+    newEvent
     isDirty:boolean = true
-    constructor (private router: Router){
+    constructor (private router: Router, private eventService: EventService) {
+        
+
 
     }
 
     cancel(){
         this.router.navigate(['/events']) //this also works instead of router link
+    }
+
+    saveEvent(formValues) {
+        this.eventService.saveEvent(formValues)
+        this.isDirty = false
+        this.router.navigate(['/events']) 
+
+        
     }
 }

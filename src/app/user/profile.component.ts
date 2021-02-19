7375479272e48025e8670fe746d2c748d5522b76
Kaushik <kaushikred293@gmail.com>
2021-02-19 15:43:09 +0530

    We saved user data to the server and made sure the app shows a message that the profile has been saved only after the server confirms it

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -41,7 +41,9 @@ export class ProfileComponent implements OnInit{
      saveProfile(formValues) {
        if(this.profileForm.valid){
         this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-        this.toastr.success('Profile Saved');
+        .subscribe(() => {
+          this.toastr.success('Profile Saved');
+        })
        }
      }
 

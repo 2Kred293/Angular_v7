603747982aa4f926f59197fb92cdbf577de442d2
Kaushik <kaushikred293@gmail.com>
2021-02-19 14:23:59 +0530

    Implemented POST method in event-service.ts and added query string paramters to searchSessions method as well to make searching easier

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable, EventEmitter } from '@angular/core'
 import { Observable, Subject, of } from 'rxjs'
 import {IEvent, ISession} from './event.model'
@@ -19,40 +19,23 @@ export class EventService{
 
   getEvent(id:number):Observable <IEvent> {
     return this.http.get<IEvent>('/api/events/'+ id)
-    .pipe(catchError(this.handleError<IEvent>('getEvents')))
+    .pipe(catchError(this.handleError<IEvent>('getEvent')))
     
   }
 
   saveEvent(event){
-    event.id=999
-    event.session = []
-    EVENTS.push(event)
+    let options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+    return this.http.post<IEvent>('/api/events', event, options) // PUT method has the same syntax but it used with update Event
+      .pipe(catchError(this.handleError<IEvent>('saveEvent')))
   }
 
-  updateEvent(event) {
-    let index = EVENTS.findIndex(x => x.id = event.id)
-    EVENTS[index] = event
+  //update event is not required as the only update allowed is one where we add sessions, we can't edit the event 
+
+  searchSessions(searchTerm: string): Observable<ISession[]> {
+    return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions')))
   }
 
-  searchSessions(searchTerm: string) {
-    var term = searchTerm.toLocaleLowerCase();
-    var results: ISession[] = [];
-
-    EVENTS.forEach(event => {
-      var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
-      matchingSessions = matchingSessions.map((session:any) => {
-        session.eventId = event.id;
-        return session;
-      })
-      results = results.concat(matchingSessions);
-    })
-
-    var emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-    return emitter;
-  }
 
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
